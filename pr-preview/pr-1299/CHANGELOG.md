4ce7512f490ba0a36c75e00802cd3eb90d812d23
eXeLearningProject <eXeLearningProject@users.noreply.github.com>
2026-02-23 13:27:00 +0000

    Remove preview for PR 1299 🛬

--- pr-preview/pr-1299/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# CHANGELOG
-
-## v4.0.0-beta1 - 2026-02-12
-
-- First beta release of eXeLearning 4.0 ready for testing and collaboration. New backend built using Elysia, Bun, and Kysely.
--- pr-preview/pr-1299/app/search/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Search module exports
- * Provides global search functionality for eXeLearning
- */
-
-export { default as GlobalSearchModal } from './GlobalSearchModal.js';
-export { stripHtmlForSearch, buildSearchIndex, search, debounce } from './SearchEngine.js';
